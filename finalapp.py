0f295d7471d015ff0c16013a26f1ee2b04708cfa
Ayaan Siddiqui <ars394@pitt.edu>
2022-07-01 20:34:20 +0530

    added download button
    dowloads data in csv format

--- finalapp.py
+++ finalapp.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from turtle import onclick
 import streamlit as st
 import pandas as pd
 
@@ -212,3 +213,17 @@ if st.sidebar.button('Show Data'):
     
     with st.expander('Raw Data'):
         st.dataframe(df)
+
+    def convert_df(df):
+        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv().encode('utf-8')
+
+    csv = convert_df(df)
+
+    st.sidebar.download_button(
+        label="Download data",
+        data=csv,
+        file_name='BitcoinData.csv',
+        mime='text/csv',
+        help='Data available as a CSV file'
+    )
